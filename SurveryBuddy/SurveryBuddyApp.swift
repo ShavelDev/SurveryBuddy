1cee08a9310bc996d8ffb80564250f098f547dab
Norbert <nor.szaw@gmail.com>
2024-01-06 22:04:56 +0100

    Initial Commit

---
+++ SurveryBuddy/SurveryBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurveryBuddyApp.swift
+//  SurveryBuddy
+//
+//  Created by Norbert Szawloga on 06/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SurveryBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
